df5c23ff0bd631543b481b7a8e8ae71f171061c2
Vlad I <17142772+attackercan@users.noreply.github.com>
2020-10-06 17:32:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ except ImportError:
     # structure of the modules.
 #     from pip._internal.download import PipSession
 #     from pip._internal.req.req_file import parse_requirements
+    pass
 
 
 def get_requirements(source):
