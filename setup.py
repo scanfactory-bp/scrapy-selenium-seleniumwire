defe155ce80bb769923aaad5d37b4b9036e1ec35
Vlad I <17142772+attackercan@users.noreply.github.com>
2020-10-07 13:55:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(
     packages=find_packages(),
-    install_requires=['scrapy>=1.0.0', 'selenium>=3.9.0']
+    install_requires=['scrapy>=1.0.0', 'selenium>=3.9.0', 'selenium-wire>=2.1.1']
 )
