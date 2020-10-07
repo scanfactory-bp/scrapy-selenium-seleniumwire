db67c56932b066dc77628afdfb4d661811a214a3
Vlad I <17142772+attackercan@users.noreply.github.com>
2020-10-07 13:51:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
 from setuptools import setup, find_packages
 
-from pip.download import PipSession
-from pip.req import parse_requirements
-
-def get_requirements(source):
-    """Get the requirements from the given ``source``
-    Parameters
-    ----------
-    source: str
-        The filename containing the requirements
-    """
-
-    install_reqs = parse_requirements(filename=source, session=PipSession())
-
-    return [str(ir.req) for ir in install_reqs]
-
 setup(
     packages=find_packages(),
-    install_requires=get_requirements('requirements/requirements.txt')
+    install_requires=['scrapy>=1.0.0', 'selenium>=3.9.0']
 )
