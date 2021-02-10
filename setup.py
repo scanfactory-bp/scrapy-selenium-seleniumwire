96997963ecc2eac26ef9caa8630d8fc6d7e96f1c
Vlad I <17142772+attackercan@users.noreply.github.com>
2021-02-10 20:28:43 +0300

    Fix packages versions. selenium-wire==2.1.1

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(
     packages=find_packages(),
-    install_requires=['scrapy>=1.0.0', 'selenium>=3.9.0', 'selenium-wire>=2.1.1']
+    install_requires=['scrapy==2.3.0', 'selenium==3.141.0', 'selenium-wire==2.1.1']
 )
